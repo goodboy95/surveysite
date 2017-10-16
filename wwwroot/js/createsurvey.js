764c9778f4bb879adfa76b9955836daeebe23628
goodboy95 <501217662@qq.com>
2017-10-16 21:55:04 +0800

    暂时交一下吧

--- wwwroot/js/createsurvey.js
+++ wwwroot/js/createsurvey.js
@@ -3,15 +3,33 @@ var quesNum = 1;
 
 function ShowChoiceBox(opt) {
     var choice = opt.value;
+    var quesId = $(opt).parents(".layui-form")[0].id;
     if (choice >= 2) {
-        $("#optionInput").show();
-        //document.getElementById("optionInput").style.visibility = "visible";
+        $(`#${quesId} #optionInput`).show();
+    }
+    else {
+        $(`#${quesId} #optionInput`).hide();
     }
 }
+
+function RemoveQues(opt) {
+    var quesId = $(opt).parents(".layui-form")[0].id;
+    console.log(quesId);
+    var thisQues = document.getElementById(quesId);
+    document.getElementById("questionBox").remove(thisQues);
+}
+
 window.onload = function(){
     layui.use("form", function(){
         var form = layui.form;
     });
+    
+    document.getElementById("addQues").onclick = function(){
+        quesNum++;
+        var quesBody = document.getElementById("q1").cloneNode(true);
+        quesBody.id = "q" + quesNum.toString();
+        document.getElementById("questionBox").appendChild(quesBody);
+    }
     //questionList.
     //document.getElementById("submit")
 };
