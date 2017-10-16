660cc90111158ca261699e4413881fa0ec368941
goodboy95 <501217662@qq.com>
2017-10-16 08:39:12 +0800

    commit

--- wwwroot/js/createsurvey.js
+++ wwwroot/js/createsurvey.js
@@ -1,5 +1,13 @@
 var questionList = new Array();
 var quesNum = 1;
+
+function ShowChoiceBox(opt) {
+    var choice = opt.value;
+    if (choice >= 2) {
+        $("#optionInput").show();
+        //document.getElementById("optionInput").style.visibility = "visible";
+    }
+}
 window.onload = function(){
     layui.use("form", function(){
         var form = layui.form;
